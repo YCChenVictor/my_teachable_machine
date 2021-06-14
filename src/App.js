3fca958e97ea5fadf0c5a259707b5248cf416934
YCChen <t5204713910@gmail.com>
2021-06-14 18:17:11 +0800

    use react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Title from './components/title/Title.js';
+
+function App() {
+  return (
+    <div>
+      <div className="App">
+        <Title />
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/title/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>Test</h1>
+      <div className="Title-Subtitle">Testing</div>
+    </div>
+  )
+}
+
+export default Title
